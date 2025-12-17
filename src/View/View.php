5d1a19583cedb2d1329a2278727ea41a6ece19d7
Author <7813306+mehov@users.noreply.github.com>
2025-12-17 19:54:57 +0000

    Pass \$options to parent

--- src/View/View.php
+++ src/View/View.php
@@ -7,7 +7,7 @@ class View extends \Cake\View\View {
     public function initialize(array $options = []): void
     {
         $this->addHelper('Icon', ['className' => 'Bakeoff/DKFDS.Icon']);
-        parent::initialize();
+        parent::initialize($options);
     }
 
 }
