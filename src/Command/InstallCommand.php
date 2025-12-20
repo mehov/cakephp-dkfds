a4fc2590c0d8f18eb6a0c6cc365a3167fc0c67d8
Author <7813306+mehov@users.noreply.github.com>
2025-12-20 15:21:43 +0000

    Refactor InstallCommand

--- src/Command/InstallCommand.php
+++ src/Command/InstallCommand.php
@@ -14,49 +14,46 @@ class InstallCommand extends \Cake\Command\Command
      */
     public function execute(\Cake\Console\Arguments $args, ConsoleIo $io): ?int
     {
-        // Make InstallCommand aware of the plugin
+        // Fix '$io must not be accessed before initialization'
+        $this->io = $io;
+
+        // Make this class aware of the plugin we're in
         $this->pluginClass = \Bakeoff\DKFDS\DKFDSPlugin::class;
         $this->plugin = new $this->pluginClass();
-        $this->pluginWebroot = $this->plugin->getPath() . 'webroot';
-        // (Re-)Link our webroot to dkfds/dkfds dist
-        $this->unlinkWebroot($io);
-        $this->linkWebroot($io);
-        $io->out();
-        $io->success('Installation completed.');
-        return static::CODE_SUCCESS;
-    }
 
-    /**
-     * Unlinks webroot in this plugin, if exists and is a symlink
-     *
-     * @param \Cake\Console\ConsoleIo $io The console io.
-     * @return void
-     */
-    public function unlinkWebroot(ConsoleIo $io): void
-    {
-        if (is_link($this->pluginWebroot)) {
-            unlink($this->pluginWebroot);
-        }
-    }
+        // Debug output
+        $io->info(sprintf('Installing %s', $this->plugin->getName()));
 
-    /**
-     * Links this plugin webroot to dkfds/dkfds dist
-     *
-     * @param \Cake\Console\ConsoleIo $io The console io.
-     * @return void
-     */
-    public function linkWebroot(ConsoleIo $io): void
-    {
-        // Actual path to dkfds/dkfds dist folder
+        /*
+         * (Re-)Link our plugin webroot to dkfds/dkfds dist folder
+         */
+        // First, remove existing link, if any
+        $this->_remove(['namespaced' => false, 'destDir' => $this->plugin->getPath(), 'link' => 'webroot']);
+        // Point our plugin webroot to dkfds/dkfds dist folder
         $vendor = \dirname(\CORE_PATH, 2);
-        $target = $vendor . '/dkfds/dkfds/dist';
-        $io->info(sprintf('Pointing %s webroot to dkfds/dkfds dist ...', $this->pluginClass));
-        $io->info(sprintf('  %s -> %s', $target, $this->pluginWebroot));
-        $result = @symlink($target, $this->pluginWebroot);
+        $dkfds_dist = $vendor . '/dkfds/dkfds/dist';
+        $plugin_webroot = $this->plugin->getPath() . 'webroot';
+        $result = $this->_createSymlink($dkfds_dist, $plugin_webroot);
         if (!$result) {
-            $io->error('... failed.');
-            $this->abort($result);
+            $io->abort("Failed creating symlink\n  TARGET $dkfds_dist\n  LINK $plugin_webroot");
+            return static::CODE_ERROR;
         }
+
+        /*
+         * (Re-)Link our plugin webroot to app webroot
+         */
+        // First, remove existing link, if any
+        $this->_remove(['namespaced' => false, 'destDir' => WWW_ROOT, 'link' => $this->plugin->getName()]);
+        // Create a shortcut to this plugin webroot in main app webroot
+        $app_webroot_plugin = WWW_ROOT . $this->plugin->getName();
+        $result = $this->_createSymlink($plugin_webroot, $app_webroot_plugin);
+        if (!$result) {
+            $io->abort("Failed creating symlink\n  TARGET $plugin_webroot\n  LINK $app_webroot_plugin");
+            return static::CODE_ERROR;
+        }
+
+        $io->success(sprintf('Installing %s completed', $this->plugin->getName()));
+        return static::CODE_SUCCESS;
     }
 
 }
